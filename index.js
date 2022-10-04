48e767ff186f13e87f924c6ad832d7c4f7c87d61
hudsonviana <hudson1206@gmail.com>
2022-10-04 01:52:43 -0300

    teste de ddeploy

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ app.get("/", (req, res) => {
   ;
 });
 
+app.get("/teste", (req, res) => {
+    let page;
+    (async () => {
+      page = await (await browserP).newPage();
+      await page.setContent(`
+      <h1>Teste</h1>  
+      <p>outro teste</p>
+      `); //teste
+      res.send(await page.content());
+    })()
+      .catch(err => res.sendStatus(500))
+      .finally(() => page.close())
+    ;
+  });
+
 app.listen(app.get("port"), () => 
   console.log("app running on port", app.get("port"))
 );
